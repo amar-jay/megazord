62ab0b519cd768bca0232b7fdff639198b45b193
amar-jay <abdelmanan.abdelrahman03@gmail.com>
2022-09-05 20:16:24 +0300

    deleted useless js

--- site/src/pages/_app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-require("../../styles/globals.css");
-function MyApp(_a) {
-    var Component = _a.Component, pageProps = _a.pageProps;
-    return <Component {...pageProps}/>;
-}
-exports["default"] = MyApp;
